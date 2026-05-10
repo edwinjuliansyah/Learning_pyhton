9d7e705c031327e39c15619423b49fce340e13ec
Emun <edwinjuliansyah273@gmail.com>
2026-05-10 12:07:25 +0700

    Refactor pricing keys, improve input validation, and fix bugs
    memperbaiki beberapa penamaan dan variabel agar lebih ringkas, dan mudah dibaca, memperbaiki validasi ketika except tercetak sehingga tidak double print, memperbaiki bug ketika input 0 dan minus tetap di jalankan perhitungannya meskipun sudah tercetak tidak valid

--- coding-practice/kalkulator-tarif-pengiriman.py
+++ coding-practice/kalkulator-tarif-pengiriman.py
@@ -1,33 +1,35 @@
 harga = {
-  "HARGA_PER_KG": 2000,
-  "HARGA_PER_KM": 3000
+  "PER_KG": 2000,
+  "PER_KM": 3000
 }
 
 surcharge = {
-    "BERAT_15KG": 5000,
-    "JARAK_50KM": 10000
+    "BERAT>15KG": 5000,
+    "JARAK>50KM": 10000
 }
 
-print(f"Harga per Kg {harga["HARGA_PER_KG"]} dan Harga per Km {harga["HARGA_PER_KM"]}\nSurcharge berat > 15Kg {surcharge["BERAT_15KG"]} dan \
-Surcharge > 50Km {surcharge["JARAK_50KM"]}\n\n")
+print(f"Harga per Kg {harga['PER_KG']} dan Harga per Km {harga['PER_KM']}\n\nSurcharge berat > 15Kg {surcharge['BERAT>15KG']} dan \
+Surcharge > 50Km {surcharge['JARAK>50KM']}\n\n")
 
-try:
-  inp1 = float(input("Masukan berat barang: "))
-  inp2 = float(input("Masukan jarak: "))
-except ValueError:
-  print("Input tidak valid, pastikan input berupa angka (berikan titik jika desimal)")
+while True:
+  try:
+    inp1 = float(input("Masukan berat barang: "))
+    inp2 = float(input("Masukan jarak pengiriman: "))
+    if inp1 <= 0 or inp2 <= 0:
+      print("Berat dan jarak harus lebih besar dari nol! (berikan titik jika desimal)")
+      continue
+    break
+  except ValueError:
+    print("Input tidak valid, pastikan input berupa angka (berikan titik jika desimal)")
 
-if inp1 <= 0 or inp2 <=0:
-  print("Angka tidak valid")
-
-biaya_berat = inp1 * harga["HARGA_PER_KG"]
-biaya_jarak = inp2 * harga["HARGA_PER_KM"]
+biaya_berat = inp1 * harga["PER_KG"]
+biaya_jarak = inp2 * harga["PER_KM"]
 total_biaya = biaya_berat + biaya_jarak
 
 if inp1 > 15:
-    total_biaya += 5000
+  total_biaya += surcharge["BERAT>15KG"]
 
 if inp2 > 50:
-  total_biaya += 10000  
+  total_biaya += surcharge["JARAK>50KM"]
 
 print(f"Total biaya: {total_biaya}")
